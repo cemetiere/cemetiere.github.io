caf8fd702b21be44c5b32d358e60c2c85f3be091
Kenny <102922055+cemetiere@users.noreply.github.com>
2022-08-04 22:51:20 +0300

    added button onclick changing color

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # WEB
-You can visit this site [HERE](https://cemetiere.github.io/)
-
-[graph](https://se.ifmo.ru/courses/web?p_p_id=iapsportletlab6_WAR_iapsportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage?1659370500036)
+ 
--- src/graph.js
+++ src/graph.js
@@ -1,3 +1,4 @@
+'use strict';
 const canvas =document.getElementById('graph');
 const ctx = canvas.getContext('2d');
 
@@ -8,83 +9,84 @@ const scaleX = 25;
 const scaleY = 25;
 
 
+function drawGraph(){
+    ctx.beginPath();
+    ctx.strokeStyle = '#f5f0e8';
 
-ctx.beginPath();
-ctx.strokeStyle = '#f5f0e8';
+    for (let i = 0; i<= canvasWidth; i = i + scaleX){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,canvasHeight);
 
-for (let i = 0; i<= canvasWidth; i = i + scaleX){
-    ctx.moveTo(i,0);
-    ctx.lineTo(i,canvasHeight);
+    }
+    ctx.stroke();
+    for (let i = 0; i<= canvasHeight; i = i + scaleY){
+        ctx.moveTo(0,i);
+        ctx.lineTo(canvasWidth,i);
 
-}
-ctx.stroke();
-for (let i = 0; i<= canvasHeight; i = i + scaleY){
-    ctx.moveTo(0,i);
-    ctx.lineTo(canvasWidth,i);
+    }
+    ctx.stroke();
+    ctx.closePath();
 
-}
-ctx.stroke();
-ctx.closePath();
+    const xCenter = Math.round(canvasWidth/scaleX/2)*scaleX;
+    const yCenter = Math.round(canvasHeight/scaleY/2)*scaleY;
 
-const xCenter = Math.round(canvasWidth/scaleX/2)*scaleX;
-const yCenter = Math.round(canvasHeight/scaleY/2)*scaleY;
+    // quadrant
+    ctx.beginPath();
+    ctx.arc(xCenter,yCenter,100,0,-Math.PI/2,true);
+    ctx.lineTo(xCenter,yCenter);
+    ctx.lineTo(xCenter+100,yCenter);
+    ctx.stroke();
+    ctx.fillStyle = "violet";
+    ctx.fill();
+    ctx.closePath();
 
-// quadrant
-ctx.beginPath();
-ctx.arc(xCenter,yCenter,100,0,-Math.PI/2,true);
-ctx.lineTo(xCenter,yCenter);
-ctx.lineTo(xCenter+100,yCenter);
-ctx.stroke();
-ctx.fillStyle = "violet";
-ctx.fill();
-ctx.closePath();
+    // rectangle
+    ctx.beginPath();
+    ctx.moveTo(xCenter,yCenter);
+    ctx.lineTo(xCenter+50,yCenter);
+    ctx.lineTo(xCenter+50,yCenter+100);
+    ctx.lineTo(xCenter,yCenter+100);
+    ctx.lineTo(xCenter,yCenter);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
-// rectangle
-ctx.beginPath();
-ctx.moveTo(xCenter,yCenter);
-ctx.lineTo(xCenter+50,yCenter);
-ctx.lineTo(xCenter+50,yCenter+100);
-ctx.lineTo(xCenter,yCenter+100);
-ctx.lineTo(xCenter,yCenter);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
+    // triangle
+    ctx.beginPath();
+    ctx.moveTo(xCenter,yCenter);;
+    ctx.lineTo(xCenter-100,yCenter);
+    ctx.lineTo(xCenter,yCenter-50);
+    ctx.lineTo(xCenter,yCenter);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
-// triangle
-ctx.beginPath();
-ctx.moveTo(xCenter,yCenter);;
-ctx.lineTo(xCenter-100,yCenter);
-ctx.lineTo(xCenter,yCenter-50);
-ctx.lineTo(xCenter,yCenter);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
+    // axes
+    ctx.beginPath();
+    ctx.strokeStyle = '#000000';
+    ctx.moveTo(xCenter,0);
+    ctx.lineTo(xCenter,canvasHeight);
+    ctx.moveTo(0,yCenter);
+    ctx.lineTo(canvasWidth,yCenter);
+    ctx.stroke();
+    ctx.closePath();
 
-
-ctx.beginPath();
-ctx.strokeStyle = '#000000';
-ctx.moveTo(xCenter,0);
-ctx.lineTo(xCenter,canvasHeight);
-ctx.moveTo(0,yCenter);
-ctx.lineTo(canvasWidth,yCenter);
-ctx.stroke();
-ctx.closePath();
-
-ctx.beginPath();
-const pointerIndent = 8;
-ctx.moveTo(xCenter-pointerIndent,pointerIndent);
-ctx.lineTo(xCenter,0);
-ctx.lineTo(xCenter+pointerIndent,pointerIndent);
-ctx.moveTo(canvasWidth-pointerIndent,yCenter-pointerIndent);
-ctx.lineTo(canvasWidth,yCenter);
-ctx.lineTo(canvasWidth-pointerIndent,yCenter+pointerIndent);
-ctx.stroke();
-ctx.closePath();
+    // axis arrows
+    ctx.beginPath();
+    const arrowIndent = 8;
+    ctx.moveTo(xCenter-arrowIndent,arrowIndent);
+    ctx.lineTo(xCenter,0);
+    ctx.lineTo(xCenter+arrowIndent,arrowIndent);
+    ctx.moveTo(canvasWidth-arrowIndent,yCenter-arrowIndent);
+    ctx.lineTo(canvasWidth,yCenter);
+    ctx.lineTo(canvasWidth-arrowIndent,yCenter+arrowIndent);
+    ctx.stroke();
+    ctx.closePath();
 
 
-
-const labels = ['-R', '-R/2', '', 'R/2', 'R'];
-ctx.fillStyle = "black";
+    // tick marks
+    const labels = ['-R', '-R/2', '', 'R/2', 'R'];
+    ctx.fillStyle = "black";
 
     for (let i=1; i<6; i++) {
         ctx.beginPath();
@@ -102,3 +104,5 @@ ctx.fillStyle = "black";
         ctx.textBaseline = 'middle';
         ctx.fillText(labels[i-1], canvasWidth/2+7, canvasHeight - i*canvasHeight/6);
     }
+}
+drawGraph();
